c88f72d8fa89407f4a05b4a376fea1cd251dd268
SockBramson <sock.bramson+github@gmail.com>
2014-07-30 09:34:08 -0500

    Create sharescan.bat

---
+++ sharescan.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+PATH=%PATH%;E:\
+:START
+SET /p inputfile="Server List: " %=%
+SET extension=%inputfile:~-3%
+SET filename=%inputfile:~0,-4%
+SET "outputfile=%filename%_Output.txt"
+IF %extension%==txt GOTO continue
+IF %extension%==TXT GOTO continue
+ECHO Incorrect entry, try again. 
+GOTO start
+:CONTINUE
+ECHO Server List is %inputfile%
+ECHO Output File is %outputfile%
+cscript.exe E:\NVOpenShares.vbs %inputfile% %outputfile%
+PAUSE
